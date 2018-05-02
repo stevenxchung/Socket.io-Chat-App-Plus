8a5ae3b9494155ab8f7f081df1f4286c556b7436
Steven <stevenxchung@gmail.com>
2018-05-02 17:02:41 -0400

    Markup added

--- Chat App/app.js
+++ Chat App/app.js
@@ -1,3 +1,4 @@
+// Import libraries
 var express = require("express"),
     app = express(),
     server = require("http").createServer(app),
@@ -5,10 +6,12 @@ var express = require("express"),
     mongoose = require("mongoose"),
     users = {};
 
+// EJS preset and directory referencing
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(express.static(__dirname + "/scripts"));
 
+// Setup Mongo Server
 mongoose.connect("mongodb://localhost/chat", function(err) {
   if (err) {
     console.log(err)``
@@ -17,6 +20,7 @@ mongoose.connect("mongodb://localhost/chat", function(err) {
   }
 });
 
+// Setup chat Schema
 var chatSchema = mongoose.Schema({
   nick: String,
   msg: String,
@@ -26,25 +30,34 @@ var chatSchema = mongoose.Schema({
   }
 });
 
+// Initialize chat schema
 var Chat = mongoose.model("Message", chatSchema);
 
+// Start server
 server.listen(process.env.PORT || 3000, function() {
   console.log("Server is live!");
 });
 
+// Render index page
 app.get("/", function(req, res) {
   res.render("index");
 });
 
+// The main interface for the chat application
 io.sockets.on("connection", function(socket) {
+
+  // Retrieves messages from database
   var query = Chat.find({});
+  // Display newest messages on the bottom (Facebook Setup)
   query.sort("-created").limit(8).exec(function(err, docs) {
     if (err) throw err;
-    console.log("Sending messages!");
+    // Send to client
     socket.emit('load msgs', docs);
   });
 
+  // When a new user joins update the list of usernames in the chatroom
   socket.on("new user", function(data, callback) {
+    // If there are no users callback false else, send data to clients
     if (data in users) {
       callback(false);
     } else {
@@ -55,14 +68,18 @@ io.sockets.on("connection", function(socket) {
     }
   });
 
+  // Sends a whisper to a user if the @ symbol is included, otherwise, send a normal message to everyone in the room
   socket.on("send message", function(data, callback) {
     var msg = data.trim();
+    // Check for @ symbol
     if (msg.substr(0, 1) === '@') {
       msg = msg.substr(1);
       var ind = msg.indexOf(" ");
+      // Check for space after the username
       if (ind !== -1) {
         var name = msg.substring(0, ind);
         var msg = msg.substring(ind + 1);
+        // Check if the user exists
         if (name in users) {
           users[name].emit("whisper", {msg: msg, nick: socket.nickname});
           console.log("Whisper!");
@@ -73,14 +90,16 @@ io.sockets.on("connection", function(socket) {
         callback("Error! Please enter a message for your whisper.")
       }
     } else {
+      // Otherwise, send a normal message to everyone in the room
       var newMsg = new Chat({msg: msg, nick: socket.nickname});
       newMsg.save(function(err) {
         if (err) throw err;
         io.sockets.emit("new message", {msg: msg, nick: socket.nickname});
-      })
+      });
     }
   });
 
+  // Removes username from the chat room when they leave
   socket.on("disconnect", function(data) {
     if (!socket.nickname) return;
     delete users[socket.nickname];
@@ -88,6 +107,7 @@ io.sockets.on("connection", function(socket) {
   });
 });
 
+// Send updated list of online users in the room to the client
 function updateNicknames() {
   io.sockets.emit("usernames", Object.keys(users));
 }
--- Chat App/scripts/message.js
+++ Chat App/scripts/message.js
@@ -1,27 +1,34 @@
+// Initialize variables and selectors
+var socket = io.connect();
+var $messageForm = $("#send-message");
+var $messageBox = $("#message");
+var $chat = $("#chat");
+
+var $nickForm = $("#setNick");
+var $nickError = $("#nickError");
+var $nickBox = $("#nickname");
+var $users = $("#users");
+
 $(function() {
-  var socket = io.connect();
-  var $messageForm = $("#send-message");
-  var $messageBox = $("#message");
-  var $chat = $("#chat");
 
-  var $nickForm = $("#setNick");
-  var $nickError = $("#nickError");
-  var $nickBox = $("#nickname");
-  var $users = $("#users");
-
+  // Where the user submits their username
   $nickForm.submit(function(e) {
     e.preventDefault();
     socket.emit("new user", $nickBox.val(), function(data) {
+      // When the user submits a name, show the chatroom and hide the username form
       if (data) {
         $("#nickWrap").hide();
         $("#contentWrap").show();
+      // Otherwise, user already exists
       } else {
         $nickError.html("That username is already taken!");
       }
     });
+    // Generate a blank form by default
     $nickBox.val("");
   });
 
+  // Display list of online users to the client
   socket.on("usernames", function(data) {
     var html = "";
     for (i = 0; i < data.length; i++) {
@@ -30,29 +37,36 @@ $(function() {
     $users.html(html);
   });
 
+  // Where the user submits their message
   $messageForm.submit(function(e) {
     e.preventDefault();
+    // Generate error if the callback is false for send message
     socket.emit("send message", $messageBox.val(), function(data, callback) {
           $chat.prepend('<span class="error">' + data + "</br>");
     });
+    // Generate a blank message by default
     $messageBox.val("");
   });
 
+  // Load messages to the client with the latest messages at the bottom
   socket.on("load msgs", function(docs) {
-    for (i = 0; i < docs.length; i++) {
+    for (i = docs.length - 1; i >= 0; i--) {
       displayMsg(docs[i]);
     }
   });
 
+  // Display new message to the client
   socket.on("new message", function(data) {
-    displayMsg();
+    displayMsg(data);
   });
 
+  // Display whisper to the client
   socket.on("whisper", function(data) {
     $chat.append('<span class="whisper"><strong>' + data.nick + ": </strong>"+ data.msg + "</br>");
   });
-
-  function displayMsg(data) {
-    $chat.append('<span class="msg"><strong>' + data.nick + ": </strong>"+ data.msg + "</br>");
-  }
 });
+
+// Display message function
+function displayMsg(data) {
+  $chat.append('<span class="msg"><strong>' + data.nick + ": </strong>"+ data.msg + "</br>");
+}
